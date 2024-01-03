206c572d7f896b9e43c956f8121e475d70ce87c3
JordiRGB <jordiraf.07@gmail.com>
2024-01-02 21:13:03 -0600

    cambios

--- ComiteFront-main/src/app/componentes/jefeisc/jefeisc.component.ts
+++ ComiteFront-main/src/app/componentes/jefeisc/jefeisc.component.ts
@@ -25,5 +25,4 @@ export class JefeiscComponent implements OnInit {
       }
     );
   }
-  
 }
--- ComiteFront-main/src/app/services/auth.service.ts
+++ ComiteFront-main/src/app/services/auth.service.ts
@@ -7,7 +7,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
+  
 })
+
 export class AuthService {
   private URL = 'http://localhost:4000/api';
 
@@ -33,13 +35,11 @@ export class AuthService {
     return this.http.post<any>(this.URL + '/users/signin', user);
   }
   registrarCaso(datosCaso: any): Observable<any> {
-    return this.http.post<any>(this.URL+ '/create/alumno',datosCaso);
+    return this.http.post<any>(this.URL+ '/create/alumno', datosCaso);
   }
 
- // Método para mostrar los datos de la colección "Alumno"
- getAlumnos(): Observable<any[]> {
-  return this.http.get<any[]>(`${this.URL}/get/alumnos`);
-}
-
-  
+  // Método para mostrar los datos de la colección "Alumno"
+  getAlumnos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.URL}/get/alumnos`);
+  }
 }
