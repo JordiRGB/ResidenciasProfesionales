0008cb4e93b02855f75f462249017bdaa9239c97
Robertobg01 <127140945+Robertobg01@users.noreply.github.com>
2024-01-03 15:41:27 -0600

    idk

---
+++ ComiteFront-main/src/app/componentes/aceptados/aceptados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aceptados',
+  templateUrl: './aceptados.component.html',
+  styleUrls: ['./aceptados.component.css']
+})
+export class AceptadosComponent {
+
+}
